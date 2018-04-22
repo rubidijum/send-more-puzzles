faf29c0b1ae646fc48b84f40dcdddd73fcd68398
Aleksandar Vracarevic <malimeda@live.com>
2018-04-22 13:56:31 +0200

    Popravljeno prosledjivanje argumenata

--- alphaSolve.py
+++ alphaSolve.py
@@ -1,8 +1,11 @@
 import re
+import constraint
+import copy
 
-
-# import constraint
-
+var = []
+variables = []
+heja = []
+firstletters = []
 
 def parseLetters(variable):
     number = []
@@ -13,82 +16,92 @@ def parseLetters(variable):
         var.append(l)
         exponent -= 1
     return (number, var)
-'''
+
 # *args -> ['a','l','p'..]
 # var -> ['a*1000', ...]
 # variables -> [alpha, beta...]
-# def constraintFunc(*args, var, variables):
-#   
-#   leftside = variables[:-1]
-#   rightside = variables[-1]
-#   idi za jednu manje
-#
-#
-#   sum = 0
-#   
-#   for left in leftside:
-#       exponent = len(left)
-#       for letter in left:
-#           sum += args[i] * 10 ** exponent
-#           exponent -= 1 
-#
-#
 
-TODO: pisemo program koji sam sebe prepravlja zapravo
 
-'''
+def constraintFunc(*args):
+  
+    leftside = variables[:-1]
+    rightside = variables[-1]
+    
+    suma = 0
+   
+    i = 0
+    recnik = dict(zip(heja,args))
+    #print args
+    print dict(zip(heja,args))
+    #print 
 
-def prepareArgs(var):
-    equals = var[-1]
-    leftside = var[:-1]
-    # print leftside
-    # print equals
+    for left in leftside:
+        #print left, "levo"
+        exponent = len(left) - 1
+        for letter in left:
+            suma += recnik[letter] * (10 ** exponent)
+            exponent -= 1 
+            i += 1
+            #print suma, "levo"
 
-    leftsum = 0
     rightsum = 0
+    rightexponent = len(rightside) - 1
+    #print rightexponent
+    #print rightside
+    for r in rightside:
+        rightsum += recnik[r] * (10 ** rightexponent)
+        rightexponent -= 1
+        #print rightsum, "desn", r
+        
+    return suma == rightsum
+        
 
-    '''
-    for ls in leftside:
-        exponent = len(ls) - 1
-        for n in ls:
-            leftsum += int(n) * (10 ** exponent)
 
-    for l in equals:
-        exponent = len(equals) - 1
-        rightsum += int(l) * (10 ** exponent)
-    '''
 
-    #print leftsum + rightsum
+# TODO: pisemo program koji sam sebe prepravlja zapravo
 
 
 if __name__ == "__main__":
-
     inputString = raw_input("Enter an expression")
 
     variables = re.split(r'\+|\=| ', inputString)
 
     letters = []
-    var = []
+    
 
     for v in variables:
-        # print v
         letters.append(parseLetters(v)[1])
         var.append(parseLetters(v)[0])
+        firstletters.append(v[0])
 
     tmp = reduce(lambda acc, el: acc + el, letters, [])
     m = reduce(lambda acc, el: acc if el in acc else acc + [el], tmp, [])
-
+    i = 0
+    heja = copy.deepcopy(m)
     if len(m) > 10:
         exit("Enter up to 10 distinct letters")
 
+    #TODO: ocisti ove grozote
+
+    #print heja
+    firstletters = reduce(lambda acc, el: acc if el in acc else acc + [el], firstletters, [])
+    for l in firstletters:
+        heja.remove(l)
+        
+    print heja
+    print firstletters
     print m
-    print var
-    print
-    print variables
-    prepareArgs(var)
+    problem = constraint.Problem()
 
-    # problem = constraint.Problem()
-
-    # problem.addVariables(m, range(0, 10))
+    problem.addVariables(heja, range(0, 10))
+    problem.addVariables(firstletters, range(1,10))
+    problem.addConstraint(constraint.AllDifferentConstraint())
+    problem.addConstraint(constraintFunc, m)
+    resenja = problem.getSolutions()
+    for r in resenja:
+        print r['s'], r['e'], r['n'], r['d'] 
+        print r['m'], r['o'], r['r'], r['e']
+        print r['m'], r['o'], r['n'], r['e'], r['y']  
+    
 
 # TODO: make multiple argumets constraint function
